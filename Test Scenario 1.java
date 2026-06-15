f5b8cb0e068d5f695374c2703456308c12a59ce4
SushmalaSabala <sushmala.sabala@gmail.com>
2026-06-15 13:29:13 -0400

    Update Test Scenario 1.java

--- Test Scenario 1.java
+++ Test Scenario 1.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SimpleFormDemoTest {
+public class TestScenario1 {
 
     @Test
     public void validateSimpleFormDemo() {
